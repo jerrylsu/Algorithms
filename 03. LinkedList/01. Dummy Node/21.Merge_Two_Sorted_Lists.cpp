1fec3b7c219e388c801a90073e94ed365661c14f
sulei <sa517301@mail.ustc.edu.cn>
2020-05-13 11:13:17 +0800

    LinkedList

--- 03. LinkedList/01. Dummy Node/21.Merge_Two_Sorted_Lists.cpp
+++ 03. LinkedList/01. Dummy Node/21.Merge_Two_Sorted_Lists.cpp
@@ -16,23 +16,23 @@ class ListNode{
 class Solution{
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
-	if(!l1 && !l2) return nullptr;
-	if(!l1 || !l2) return !l1 ? l2 : l1;
+	    if(!l1 && !l2) return nullptr;
+	    if(!l1 || !l2) return !l1 ? l2 : l1;
 
-	ListNode dummy(-1), *current;
-	current = &dummy;
-	while(l1 && l2){
-	    if(l1->val < l2->val){
-	        current->next = l1;
-		l1 = l1->next;
-	    }else{
-	        current->next = l2;
-		l2 = l2->next;
+	    ListNode dummy(-1), *current;
+	    current = &dummy;
+	    while(l1 && l2){
+	        if(l1->val < l2->val){
+	            current->next = l1;
+		    l1 = l1->next;
+	        }else{
+	            current->next = l2;
+		    l2 = l2->next;
+	        }
+	        current = current->next;
 	    }
-	    current = current->next;
-	}
-	current->next = l1 == nullptr ? l2 : l1;
-	return dummy.next;
+	    current->next = l1 == nullptr ? l2 : l1;
+	    return dummy.next;
     }
 
     void printLinkedList(ListNode* l){
