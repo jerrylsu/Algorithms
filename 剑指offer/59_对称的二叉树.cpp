7a1b0577d456967a29bab35ab491a2fdd3629c8a
sulei <sa517301@mail.ustc.edu.cn>
2020-05-26 14:53:18 +0800

    tree

--- 剑指offer/59_对称的二叉树.cpp
+++ 剑指offer/59_对称的二叉树.cpp
@@ -11,14 +11,17 @@ class TreeNode {
 class Solution {
 public:
     bool isSymmetrical(TreeNode* root){
-        if(root == nullptr) return true;
-        return helper(root->left, root->right);
+        return root ? isSymmetrical(root->left, root->right) : true;
     }
 
-    bool helper(TreeNode* node1, TreeNode* node2){
+    bool isSymmetrical(TreeNode* node1, TreeNode* node2){
+        // case 1: 两个子树均为空
         if(node1 == nullptr && node2 == nullptr) return true;
+        // case 2: 存在一个子树为空
         if(node1 == nullptr || node2 == nullptr) return false;
+        // case 3：两个子树根结点的值不相等
         if(node1->val != node2->val) return false;
-        return helper(node1->left, node2->right) && helper(node1->right, node2->left);
+        // 递归处理
+        return isSymmetrical(node1->left, node2->right) && helper(node1->right, node2->left);
     }
 };
