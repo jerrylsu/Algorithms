1d4606130ec0334f5d2be70f8ee1dae391c1a5ff
jerry <sa517301@mail.ustc.edu.cn>
2021-04-11 20:19:48 +0800

    edit distance

---
+++ 16_DynamicProgramming/Subsequence_type/72.Edit_Distance.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        def ed(m, n):
+            if m == -1:
+                return n + 1
+            if n == -1:
+                return m + 1
+            if word1[m] == word2[n]:
+                return ed(m-1, n-1)
+            else:
+                return min(ed(m-1, n), ed(m-1, n-1), ed(m, n-1)) + 1
+        m, n = len(word1) - 1, len(word2) - 1
+        return ed(m, n)
+
+    def minDistance(self, word1: str, word2: str) -> int:
+        mem = {}
+        def ed(m, n):
+            if (m, n) in mem:
+                return mem[(m, n)]
+            if m == -1:
+                return n + 1
+            if n == -1:
+                return m + 1
+            if word1[m] == word2[n]:
+                mem[(m, n)] = ed(m-1, n-1)
+            else:
+                mem[(m, n)] = min(ed(m-1, n), ed(m-1, n-1), ed(m, n-1)) + 1
+            return mem[(m, n)]
+        m, n = len(word1) - 1, len(word2) - 1
+        return ed(m, n)
