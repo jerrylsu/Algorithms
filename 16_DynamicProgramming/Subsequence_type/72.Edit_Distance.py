44eca4ba7c6211f49e1fcf6544abb1bef4b99dbf
jerry <sa517301@mail.ustc.edu.cn>
2021-04-15 09:09:44 +0800

    edit distance dp

--- 16_DynamicProgramming/Subsequence_type/72.Edit_Distance.py
+++ 16_DynamicProgramming/Subsequence_type/72.Edit_Distance.py
@@ -37,3 +37,20 @@ class Solution:
             return mem[(m, n)]
         m, n = len(word1) - 1, len(word2) - 1
         return ed(m, n)
+
+    def minDistance(self, word1: str, word2: str) -> int:
+        """DP. Bottom to Top.
+        """
+        m, n = len(word1) + 1, len(word2) + 1
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(n):
+            dp[0][i] = i
+        for i in range(m):
+            dp[i][0] = i
+        for i in range(1, m):
+            for j in range(1, n):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + 1
+        return dp[1][-1]
