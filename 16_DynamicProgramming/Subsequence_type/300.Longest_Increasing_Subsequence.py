d1939da7a0bc6de1ad226bd845d2e75bc3706438
jerry <sa517301@mail.ustc.edu.cn>
2021-04-18 18:12:07 +0800

    DP LIS

--- 16_DynamicProgramming/Subsequence_type/300.Longest_Increasing_Subsequence.py
+++ 16_DynamicProgramming/Subsequence_type/300.Longest_Increasing_Subsequence.py
@@ -1,5 +1,9 @@
 class Solution:
+    """https://leetcode.com/problems/longest-increasing-subsequence/discuss/1008117/Python-3-Brute-force(Recursive)-Recursion-%2B-Memo-DP-Binary_search
+    """
     def lengthOfLIS(self, nums: list) -> int:
+        """DP
+        """
         n = len(nums)
         dp = [1] * n
         for i in range(1, n):
